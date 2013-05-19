7c39e256d704f7e4bc1a7ae73b3c1d1c56562f35
bigbassroller <bigbassroller@gmail.com>
2013-05-19 04:45:02 -0700

    Worked on single blog page (content-single)
    Added "shame.less", a file for hacks that should be fixed later.

--- content-single.php
+++ content-single.php
@@ -4,56 +4,40 @@
  */
 ?>
 <!-- Begin content-single.php -->
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post" <?php post_class(); ?>>
+	
 	<header class="entry-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
-
-		<div class="entry-meta">
+		<h1 class="blog-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'space_rocket' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+		<?php if ( 'post' == get_post_type() ) : ?>
+		<p class="entry-meta">
 			<?php space_rocket_posted_on(); ?>
-		</div><!-- .entry-meta -->
+		</p><!-- .entry-meta -->
+		<?php endif; ?>
+		<?php if ( has_post_thumbnail() ) : ?>
+<div class="full-img-wrap">
+	<?php the_post_thumbnail(); ?>
+</div>
+	<?php endif; ?>
+
+		
+		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
+		<span class="sep"> | </span>
+		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'space_rocket' ), __( '1 Comment', 'space_rocket' ), __( '% Comments', 'space_rocket' ) ); ?></span>
+		<?php endif; ?>
 	</header><!-- .entry-header -->
 
+	<?php if ( is_search() ) : // Only display Excerpts for Search ?>
+	<div class="entry-summary">
+		<?php the_excerpt(); ?>
+	</div><!-- .entry-summary -->
+	<?php else : ?>
 	<div class="entry-content">
-		<?php the_content(); ?>
+		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'space_rocket' ) ); ?>
 		<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'space_rocket' ), 'after' => '</div>' ) ); ?>
 	</div><!-- .entry-content -->
+	<?php endif; ?>
 
-	<footer class="entry-meta">
-		<?php
-			/* translators: used between list items, there is a space after the comma */
-			$category_list = get_the_category_list( __( ', ', 'space_rocket' ) );
-
-			/* translators: used between list items, there is a space after the comma */
-			$tag_list = get_the_tag_list( '', __( ', ', 'space_rocket' ) );
-
-			if ( ! space_rocket_categorized_blog() ) {
-				// This blog only has 1 category so we just need to worry about tags in the meta text
-				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was tagged %2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'space_rocket' );
-				} else {
-					$meta_text = __( 'Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'space_rocket' );
-				}
-
-			} else {
-				// But this blog has loads of categories so we should probably display them here
-				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'space_rocket' );
-				} else {
-					$meta_text = __( 'This entry was posted in %1$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'space_rocket' );
-				}
-
-			} // end check for categories on this blog
-
-			printf(
-				$meta_text,
-				$category_list,
-				$tag_list,
-				get_permalink(),
-				the_title_attribute( 'echo=0' )
-			);
-		?>
-
-		<?php edit_post_link( __( 'Edit', 'space_rocket' ), '<span class="edit-link">', '</span>' ); ?>
-	</footer><!-- .entry-meta -->
 </article><!-- #post-## -->
+<div class="blog-dotted-line"></div>
+<div class="blog-dotted-line"></div>
 <!-- End content-single.php -->
